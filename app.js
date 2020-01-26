120a625969f7b0229616339bf5074ce43615fbc6
woc917 <woc917@hotmail.com>
2020-01-26 18:31:09 +0900

    add morgan nodemon bodyParser

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
 
 
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 
